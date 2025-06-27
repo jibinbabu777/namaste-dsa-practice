c7ea8a011073ee9e76ccb7b1198194c9f05966ec
jibinbabukummil@gmail.com <jibinbabukummil@gmail.com>
2025-06-27 09:37:09 +0530

    array problem

---
+++ Array/sellStock.js
@@ -0,0 +1,19 @@
+
+
+function sellStock(arr){
+    let min = arr[0];
+    let maxProfit = 0;
+    for(let i=1; i<arr.length; i++){
+        if(arr[i]-min>maxProfit){
+            maxProfit = arr[i]-min;
+        }
+        if(arr[i]<min){
+            min= arr[i]
+        }
+    }
+    return maxProfit
+}
+
+let arr = [7,1,5,3,6,4];
+const result = sellStock(arr)
+console.log(result)
